40735f728c324f42a5a12c7ad4d08ed5b746fedf
AMOB-Kahuna <balloabdulmujeeb42@gmail.com>
2025-06-13 00:42:17 +0100

    Make request to api to get questions and display them with the Question.jsx component dynamically. use html entities package to decode html entities in api response and merge the correct answer into the incorrect answers array the sort before rendering on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {decode} from 'html-entities'
 import './App.css'
 import Home from './Home'
 import Question from './Question'
@@ -6,9 +7,19 @@ import Question from './Question'
 function App() {
 
   const [quizStarted, setQuizStarted] = useState(false)
+  const [quizData, setQuizData] = useState()
+  
+
 
   function startQuiz() {
     setQuizStarted(true)
+
+    fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
+      .then(res => res.json())
+      .then(data => {
+        setQuizData(data.results)
+      })
+      .catch(err => console.error('Error fetching quiz data:', err))
   }
 
   return (
@@ -16,32 +27,21 @@ function App() {
       {
         quizStarted ?
           <main>
-            <Question
-              question="How would one say goodbye in Spanish?"
-              options={['Adiós', 'Hola', 'Au Revoir', 'Salir']}
-            />
-
-            <Question
-              question="How would one say goodbye in Spanish?"
-              options={['Adiós', 'Hola', 'Au Revoir', 'Salir']}
-            />
-
-            <Question
-              question="How would one say goodbye in Spanish?"
-              options={['Adiós', 'Hola', 'Au Revoir', 'Salir']}
-            />
-
-            <Question
-              question="How would one say goodbye in Spanish?"
-              options={['Adiós', 'Hola', 'Au Revoir', 'Salir']}
-            />
-
-            <Question
-              question="How would one say goodbye in Spanish?"
-              options={['Adiós', 'Hola', 'Au Revoir', 'Salir']}
-            />
+            {quizData && quizData.map( (item, index) => {
+              const question = item.question
+              let options = [...item.incorrect_answers, item.correct_answer]
+              options = options.sort(() => Math.random() - 0.5)
+              return (
+                <Question
+                  key={index}
+                  question={decode(question)}
+                  options={options}
+                />
+              )
+            })}
 
             <button className="btn check-btn">Check Answers</button>
+
           </main> :
             <Home onClick={startQuiz} />
         }
